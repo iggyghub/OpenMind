4acd1ce7b1cab9777eb4033024502537c1d4113a
iggyghub <iggyghub@users.noreply.github.com>
2026-08-22 16:44:56 -0400

    self-dev: Implement ONLY this slice exactly as the issue specifies:
    # S5a: Alpaca

--- cerebral/trading/__init__.py
+++ cerebral/trading/__init__.py
@@ -1,5 +1,6 @@
 from .cost_model import SpreadCostModel, apply_costs_to_returns, compute_backtest_result, BacktestResult, Trade
 from .gauntlet import oos_test, walk_forward, GateResult, run_gauntlet, StrategyCard, GauntletGateResult
+from .broker import BrokerClient, AlpacaBrokerClient, StubBrokerClient, Account, Position, Order, Side, OrderType
 
 __all__ = [
     "SpreadCostModel",
@@ -13,4 +14,12 @@ __all__ = [
     "run_gauntlet",
     "StrategyCard",
     "GauntletGateResult",
+    "BrokerClient",
+    "AlpacaBrokerClient",
+    "StubBrokerClient",
+    "Account",
+    "Position",
+    "Order",
+    "Side",
+    "OrderType",
 ]
